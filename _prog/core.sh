d7456017d7139e5d3d5e639df439d0c905f715d7
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-09-09 15:05:12 -0400

    Operational.

--- _prog/core.sh
+++ _prog/core.sh
@@ -1,11 +1,16 @@
 ##### Core
 
-resetPulse() {
+_resetPulse() {
+	pgrep kmix >/dev/null 2>&1 && resetKmix="true"
+	
 	pkill pulse
 	pax11publish -r
 	start-pulseaudio-x11
 	sleep 1
-	pkill kmix
-	kmix >/dev/null 2>&1 &
 	
+	if [[ "$resetKmix" == "true" ]]
+	then
+		pkill kmix
+		kmix >/dev/null 2>&1 &
+	fi
 }
