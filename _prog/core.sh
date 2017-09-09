7af5df077ff99e34da8f6d75d93f8916632625ce
mirage335 <spamfreemirage335@gmail.com>
2017-09-09 16:01:42 -0400

    Stability fixes.

--- _prog/core.sh
+++ _prog/core.sh
@@ -3,10 +3,14 @@
 _resetPulse() {
 	pgrep kmix >/dev/null 2>&1 && resetKmix="true"
 	
-	pkill pulse
+	pkill pulse >/dev/null 2>&1
+	pgrep pulse >/dev/null 2>&1 && sleep 1
+	pkill -KILL >/dev/null 2>&1 pulse
+	pgrep pulse >/dev/null 2>&1 && sleep 1
+	
 	pax11publish -r
 	start-pulseaudio-x11
-	sleep 1
+	sleep 5
 	
 	if [[ "$resetKmix" == "true" ]]
 	then
